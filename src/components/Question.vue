55f4b783897e2165a244f9970238adaa5d440908
zhshakib <zhshakib@gmail.com>
2021-12-07 12:10:24 +0600

    initial Commit

---
+++ src/components/Question.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg-white p-12 rounded-lg shadow-lg w-full mt-8">
+    <div>
+      <p class="text-2xl font-bold">{{ title }}</p>
+      <label for="a" class=" block mt-4 border border-gray-300 rounded-lg py-2 px-6 text-lg">
+        <input v-for="choice, key in choices" :key="key" type="radio" class=" hidden"> {{choice}}
+      </label>
+
+    </div>
+  </div>
+</template>
+
+<script>
+;
+export default {
+  name: 'Question',
+  props: ['title', 'choices']
+  
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,34 @@
 <template>
-  <div class="home">
-    <h2 class="text-center font-bold">Hello!</h2>
+  <div class="antialiased text-gray-700 bg-gray-100">
+    <div class="flex w-full h-screen justify-center items-center">
+      <div class="w-full max-w-xl">
+        <h1 class=" font-bold text-5xl text-center text-indigo-700">
+          Simple Quiz
+        </h1>
+        <question :title="questions[0]['question']" :choices="questions[0]['incorrect_answer'].map(a=>a)"></question>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
+import axios from 'axios';
+import Question from "../components/Question.vue"
 export default {
   name: 'Home',
+  components:{
+    Question
+  },
+  mounted(){
+    axios.get('https://opentdb.com/api.php?amount=10&difficulty=hard&type=multiple').then((res) => {
+      this.questions = res.data.results
+    })
+  },
+  data(){
+    return{
+      questions: [],
+      selectedAns: "",
+    }
+  },
 };
 </script>
